ebdc50489175821841f9d2d2527a78e3886ab23d
kawabatakouhei <kohei.kawabata@mac.com>
2017-06-05 13:23:50 +0900

    Add dockerfile for constructing apache, postgres server.

---
+++ sample-docker/build_postgres/scripts/init_db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set PASSWORD=postgres
+
+su - postgres -c "pg_ctl start -w;psql -f /var/local/postgresql/scripts/create_role.sql;pg_ctl stop -m fast"
+su - postgres -c "pg_ctl start -w;psql -f /var/local/postgresql/scripts/create_db.sql;pg_ctl stop -m fast"
+su - postgres -c "pg_ctl start -w;psql -f /var/local/postgresql/scripts/create_table.sql -d sample_app;pg_ctl stop -m fast"
+su - postgres -c "pg_ctl start -w;psql -f /var/local/postgresql/scripts/grant_role.sql -d sample_app;pg_ctl stop -m fast"
